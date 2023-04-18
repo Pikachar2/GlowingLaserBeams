91d1f915d098301f6d4d2b6f528fdaa5d7d54ec0
Pikachar2 <highiq2@hotmail.com>
2023-04-18 07:28:53 -0600

    In progress changes for Disco Lights

--- GlowingLaserBeams/data-updates.lua
+++ GlowingLaserBeams/data-updates.lua
@@ -11,22 +11,22 @@ local graphics_location = "__GlowingLaserBeams__/graphics/"
 local laserBeam = "laser-beam"
 
 
-	--if settings.startup["laserfix-color"].value == "'Disco'" then
-	--	r = 1
-	--	g = 1
-	--	b = 1
-	--	glow_r = 1
-	--	glow_g = 1
-	--	glow_b = 1
-	--	path = "disco/"
-	--else
-		r = settings.startup["laserfix-r"].value
-		g = settings.startup["laserfix-g"].value
-		b = settings.startup["laserfix-b"].value
-		glow_r=settings.startup["laserfix-glow-r"].value
-		glow_g=settings.startup["laserfix-glow-g"].value
-		glow_b=settings.startup["laserfix-glow-b"].value
-	--end
+if settings.startup["laserfix-color"].value == "'Disco'" then
+	r = 1
+	g = 1
+	b = 1
+	glow_r = 1
+	glow_g = 1
+	glow_b = 1
+	path = "disco/"
+else
+	r = settings.startup["laserfix-r"].value
+	g = settings.startup["laserfix-g"].value
+	b = settings.startup["laserfix-b"].value
+	glow_r=settings.startup["laserfix-glow-r"].value
+	glow_g=settings.startup["laserfix-glow-g"].value
+	glow_b=settings.startup["laserfix-glow-b"].value
+end
 	
 	data.raw.beam[laserBeam].head =
 	{
@@ -36,9 +36,9 @@ local laserBeam = "laser-beam"
 			table.deepcopy(data.raw.beam[laserBeam].head)
 		}
 	}
-	data.raw.beam[laserBeam].head.layers[1].filename = graphics_location .. "beam-body-color.png"
+	data.raw.beam[laserBeam].head.layers[1].filename = graphics_location .. path .. "beam-body-color.png"
 	data.raw.beam[laserBeam].head.layers[1].tint = {r=r,g=g,b=b}
-	data.raw.beam[laserBeam].head.layers[2].filename = graphics_location .. "beam-body-light.png"
+	data.raw.beam[laserBeam].head.layers[2].filename = graphics_location .. path .. "beam-body-light.png"
 	
 	data.raw.beam[laserBeam].tail =
 	{
@@ -48,9 +48,9 @@ local laserBeam = "laser-beam"
 			table.deepcopy(data.raw.beam[laserBeam].tail)			
 		}
 	}
-	data.raw.beam[laserBeam].tail.layers[1].filename = graphics_location .. "beam-end-color.png"
+	data.raw.beam[laserBeam].tail.layers[1].filename = graphics_location .. path .. "beam-end-color.png"
 	data.raw.beam[laserBeam].tail.layers[1].tint = {r=r,g=g,b=b}
-	data.raw.beam[laserBeam].tail.layers[2].filename = graphics_location .. "beam-end-light.png"
+	data.raw.beam[laserBeam].tail.layers[2].filename = graphics_location .. path .. "beam-end-light.png"
 	
 	data.raw.beam[laserBeam].body =
     {
@@ -60,9 +60,10 @@ local laserBeam = "laser-beam"
 			table.deepcopy(data.raw.beam[laserBeam].body[1])			
 		}
     }
-data.raw.beam[laserBeam].body.layers[1].filename = graphics_location .. "beam-body-color.png"
+	
+data.raw.beam[laserBeam].body.layers[1].filename = graphics_location .. path .. "beam-body-color.png"
 data.raw.beam[laserBeam].body.layers[1].tint = {r=r,g=g,b=b}
-data.raw.beam[laserBeam].body.layers[2].filename = graphics_location .. "beam-body-light.png"
+data.raw.beam[laserBeam].body.layers[2].filename = graphics_location .. path .. "beam-body-light.png"
 
 if settings.startup["laserfix-doubletint"].value then
 	data.raw.beam[laserBeam].head.layers[2].tint = {r=r,g=g,b=b}
@@ -75,5 +76,5 @@ data.raw.beam[laserBeam].ground_light_animations.tail.tint = {r=glow_r,g=glow_g,
 data.raw.beam[laserBeam].ground_light_animations.body.tint = {r=glow_r,g=glow_g,b=glow_b}
 
 
-data.raw.beam[laserBeam].light_animations.head.filename = graphics_location .. "hr-laser-body-light.png"
-data.raw.beam[laserBeam].light_animations.body[1].filename = graphics_location .. "hr-laser-body-light.png"
+data.raw.beam[laserBeam].light_animations.head.filename = graphics_location .. path .. "hr-laser-body-light.png"
+data.raw.beam[laserBeam].light_animations.body[1].filename = graphics_location .. path .. "hr-laser-body-light.png"
